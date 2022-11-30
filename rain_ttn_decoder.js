0661ed1479e9bf78f23acf57c4f43dead32fad53
christianpaetz <christianpaetz@users.noreply.github.com>
2022-11-30 18:29:26 +0100

    Update rain_ttn_decoder.js
    display some values as float, fix 2-complement display on temperature value, config values now in config array

--- rain_ttn_decoder.js
+++ rain_ttn_decoder.js
@@ -10,35 +10,28 @@ function decodeUplink(input) {
       		case 0x04:
         		p = bytes[++i];
         		v = (bytes[++i] << 8)+  bytes[++i];
-        		switch (p) {
-				case 0x02:
-					data.conf_heartbeat=v;
-        	        break;
-   		     	case 0x03:
-        	    	data.conf_heavyrain=v;
-            	    break;
-        		case 0x04:
-        			data.conf_interval=v;
-            		break;
-            	default:
-                	data.error = "config parameter? "+bytes[i];
-        		}
-        	break;
+        		data.config = {};
+        		data.config[p]=v;
+        		break;
         	case 0x06:
         		sensor = bytes[++i];
             	sensorvalue = (bytes[++i] << 8)+  bytes[++i];
             	switch(sensor) {
             	case 0x01:
-                	data.temperature = sensorvalue;
+            		if(sensorvalue > 32768) sensorvalue -= 65536
+                	data.temperature = sensorvalue/10.0; 
+                    break;
+            	case 0x02:
+                	data.humidity = sensorvalue;
                     break;
             	case 0x03:
                 	data.uptime = sensorvalue;
                     break;
             	case 0x81:
-                	data.rainlevel = sensorvalue;
+                	data.rainlevel = sensorvalue*0.5;
                     break;
                 default:
-                    data.error = "sensor type? "+bytes[i];
+                    data.error = "sensor type? "+sensor;
             	}
         	break;
         case 0x0a:
@@ -50,8 +43,8 @@ function decodeUplink(input) {
             data.alarm_value = (bytes[++i] << 8)+  bytes[++i];
             break;
         case 0x12:
-            data.bat_voltage = bytes[++i];
-            data.bat_consumption = (bytes[++i] << 8) + bytes[++i];        
+            data.bat_volt = bytes[++i]/10.0;
+            data.bat_mAh = (bytes[++i] << 8) + bytes[++i];        
             break;
         default:
             data.error = "command? "+bytes[i];
